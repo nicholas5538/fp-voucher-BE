b95a0216da9f29c1c3bb6042096bdd70ce988426
nicholas5538 <nicholas5538@gmail.com>
2023-08-04 13:45:44 +0800

    Create function to connect MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import { config } from "dotenv";
 import express from "express";
+import connectDb from "./db/connect";
 
 config();
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT ?? 3001;
+const uri = process.env.MONGO_URI ?? "";
 
 app.use(express.json());
 
 (async () => {
   try {
-    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+    await connectDb({ uri, collection: "fp-capstone-backend" });
   } catch (err) {
     console.log(err);
   }
-})();
+})().then(() => {
+  app.listen(() => console.log(`Server is listening on port ${port}.`));
+});
