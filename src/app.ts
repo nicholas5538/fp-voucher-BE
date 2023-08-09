e26aded66a21a960176985187f834ce03d0874a1
nicholas5538 <nicholas5538@gmail.com>
2023-08-09 12:28:06 +0800

    Implement GET requests for vouchers
    Get vouchers (including pagination) and Get voucher request

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import { config } from "dotenv";
-import express from "express";
+import express, { type Application } from "express";
 import connectDb from "./db/connect";
+import errorHandlerMiddleware from "./middleware/error-handler";
+import notFound from "./middleware/not-found";
+import router from "./routes/vouchers";
 
 config();
-const app = express();
+const app: Application = express();
 const port = process.env.PORT ?? 3001;
 const uri = process.env.MONGO_URI ?? "";
 
+// Middleware
 app.use(express.json());
+
+// Routes
+app.use("/api/v1/vouchers", router);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 (async () => {
   try {
@@ -16,5 +25,5 @@ app.use(express.json());
     console.log(err);
   }
 })().then(() => {
-  app.listen(() => console.log(`Server is listening on port ${port}.`));
+  app.listen(port, () => console.log(`Server is listening on port ${port}.`));
 });
