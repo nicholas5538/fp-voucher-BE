320ba80db5a11febc1fc95c64afe7018d4505a26
nicholas5538 <nicholas5538@gmail.com>
2023-08-09 12:23:12 +0800

    Fix typo error on error middleware
    Missed out next as one of the parameters for the error middleware

--- src/middleware/not-found.ts
+++ src/middleware/not-found.ts
@@ -1,8 +1,7 @@
 import { expressFn } from "../constants/type-interface";
 import createError from "http-errors";
 
-const notFound: expressFn<void> = (_req, _res, next) => {
-  return next(new createError.NotFound());
-};
+const notFound: expressFn<void> = (_req, _res, next) =>
+  next(new createError.NotFound());
 
 export default notFound;
