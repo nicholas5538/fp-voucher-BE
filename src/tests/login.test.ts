3b39ecbcaf70868f315fa32e1bb2ae8155d1f246
nicholas5538 <nicholas5538@gmail.com>
2023-08-12 12:59:39 +0800

    Destructure all request

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -8,50 +8,51 @@ const dummyBody: Readonly<{ [key: string]: string }> = {
 
 describe("POST /user endpoint", () => {
   it("should return 400 when email is not provided", async () => {
-    const response = await request(app)
+    const { badRequest, body, statusCode } = await request(app)
       .post("/user")
       .send({ name: dummyBody.name });
 
-    expect(response.statusCode).toBe(400);
-    expect(response.badRequest).toBeTruthy();
-    expect(response.body.msg).toBe("Email is missing from the request");
+    expect(statusCode).toBe(400);
+    expect(badRequest).toBeTruthy();
+    expect(body.msg).toBe("Email is missing from the request");
   });
 
   it("should return 400 when email is not @gmail.com", async () => {
-    const response = await request(app)
+    const { badRequest, body, statusCode } = await request(app)
       .post("/user")
       .send({ email: "123@outlook.com", name: dummyBody.name });
-    expect(response.statusCode).toBe(400);
-    expect(response.badRequest).toBeTruthy();
-    expect(response.body.msg).toBe("Email does not match a gmail format");
+    expect(statusCode).toBe(400);
+    expect(badRequest).toBeTruthy();
+    expect(body.msg).toBe("Email does not match a gmail format");
   });
 
   it("should return 400 when name is not provided", async () => {
-    const response = await request(app)
+    const { badRequest, body, statusCode } = await request(app)
       .post("/user")
       .send({ email: dummyBody.email });
 
-    expect(response.statusCode).toBe(400);
-    expect(response.badRequest).toBeTruthy();
-    expect(response.body.msg).toBe("Name is missing from the request");
+    expect(statusCode).toBe(400);
+    expect(badRequest).toBeTruthy();
+    expect(body.msg).toBe("Name is missing from the request");
   });
 
   it("should return 400 when name is too long", async () => {
-    const response = await request(app).post("/user").send({
-      email: dummyBody.email,
-      name: "2ij1ifjiu4ghiu3hgiu4hgu4hguyh2uyhuy2gbyu3bf3bf4tasaass3f3wf4",
-    });
-    expect(response.statusCode).toBe(400);
-    expect(response.badRequest).toBeTruthy();
-    expect(response.body.msg).toBe(
-      "Name should be less than 30 characters long"
-    );
+    const { badRequest, statusCode, body } = await request(app)
+      .post("/user")
+      .send({
+        email: dummyBody.email,
+        name: "2ij1ifjiu4ghiu3hgiu4hgu4hguyh2uyhuy2gbyu3bf3bf4tasaass3f3wf4",
+      });
+    expect(statusCode).toBe(400);
+    expect(badRequest).toBeTruthy();
+    expect(body.msg).toBe("Name should be less than 30 characters long");
   });
 
   it("should return 201 and generate new token", async () => {
-    const response = await request(app).post("/user").send(dummyBody);
-
-    expect(response.statusCode).toBe(201);
-    expect(response.body).not.toBeNull();
+    const { body, statusCode } = await request(app)
+      .post("/user")
+      .send(dummyBody);
+    expect(statusCode).toBe(201);
+    expect(body).not.toBeNull();
   });
 });
