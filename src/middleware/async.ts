2807b549a30a1fdfc9ea356e48e116e9f751d42e
Nicholas Yong <nicholas8399@gmail.com>
2023-12-27 14:08:18 +0800

    Declare explicit type declaration
    Remove any from type and replace it with explicit type declaration

--- src/middleware/async.ts
+++ src/middleware/async.ts
@@ -1,12 +1,9 @@
 import type { Request, Response, NextFunction } from "express";
 import { expressFn } from "../constants/type-interface.js";
 
-const asyncWrapper = (fn: expressFn<Promise<any>>) => {
-  return async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => await fn(req, res, next).catch((err) => next(err));
+const asyncWrapper = (fn: expressFn<Promise<void | Response>>) => {
+  return async (req: Request, res: Response, next: NextFunction) =>
+    await fn(req, res, next).catch((err) => next(err));
 };
 
 export default asyncWrapper;
