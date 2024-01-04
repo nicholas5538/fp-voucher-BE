33f4ebaafa1208cb1d0faf3b98c8031b33e8a3ad
Nicholas Yong <nicholas8399@gmail.com>
2024-01-04 17:31:02 +0800

    Add prisma disconnection on middleware

--- src/middleware/async.ts
+++ src/middleware/async.ts
@@ -1,9 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
 import { expressFn } from "../constants/type-interface.js";
+import { PrismaClient } from "@prisma/client";
 
+export const prisma = new PrismaClient();
 const asyncWrapper = (fn: expressFn<Promise<void | Response>>) => {
   return async (req: Request, res: Response, next: NextFunction) =>
-    await fn(req, res, next).catch((err) => next(err));
+    await fn(req, res, next)
+      .catch((err) => next(err))
+      .finally(async () => await prisma.$disconnect());
 };
 
 export default asyncWrapper;
