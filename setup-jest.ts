00e374a36ee31f3048cb8417cb35c478114bde31
Nicholas Yong <nicholas8399@gmail.com>
2024-01-05 15:23:00 +0800

    Change jest setup

--- setup-jest.ts
+++ setup-jest.ts
@@ -2,13 +2,11 @@ import { prisma } from "./src/middleware/async.js";
 import server from "./src/index.js";
 import { dummyBody } from "./src/tests/common.js";
 
-afterEach(() => server.close());
 afterAll(async () => {
-  await prisma.user.delete({
-    where: {
-      email: dummyBody.email,
-      name: dummyBody.name,
-    },
-  });
+  await prisma.$executeRaw`DELETE
+                           FROM "public"."User"
+                           WHERE email = ${dummyBody.email}
+                             AND name = ${dummyBody.name}`;
   await prisma.$disconnect();
+  server.close();
 });
