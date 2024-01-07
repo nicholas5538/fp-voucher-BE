ed2eac7ac5dac3109f462fc500ff9f5df52e6666
Nicholas Yong <nicholas8399@gmail.com>
2024-01-07 12:47:23 +0800

    Change server close codes

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,9 @@ const server = app.listen(port, () =>
   console.log(`Server is ready on port ${port}`)
 );
 
-process.on("SIGTERM", () => {
-  console.info("SIGTERM signal received.");
-  console.log("Closing http server.");
-  server.close((err) => {
-    console.log("Http server closed.");
-    process.exit(err ? 1 : 0);
-  });
+server.close((err) => {
+  console.log("Server is closed");
+  process.exit(err ? 1 : 0);
 });
 
 export default server;
