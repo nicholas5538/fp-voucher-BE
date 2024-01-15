2348234987b592ba9711f64e37429b9adb943c55
Nicholas Yong <nicholas8399@gmail.com>
2024-01-15 13:18:20 +0800

    Implement basic redis caching for login process

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
+import * as crypto from "crypto";
 import express, { type Application } from "express";
+import session, { type SessionOptions } from "express-session";
 import { OAuth2Client } from "google-auth-library";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFound from "./middleware/not-found.js";
@@ -7,16 +9,34 @@ import googleRouter from "./routes/google-auth.js";
 import loginRouter from "./routes/login.js";
 import voucherRouter from "./routes/vouchers.js";
 
-const app: Application = express();
 export const oAuth2Client = new OAuth2Client(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   "postmessage"
 );
 
-// Middleware
-app.use(express.json());
+const app: Application = express();
+const productionMode = app.get("env") === "production";
+const sess = {
+  genid(): string {
+    return crypto.randomUUID();
+  },
+  secret: process.env.SESSION_SECRET!,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    sameSite: productionMode ? "lax" : "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
+} satisfies SessionOptions;
+
+app.set("trust proxy", productionMode ? 1 : 0);
 app.use(cors());
+app.use(session(sess));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
 app.use("/auth/google", googleRouter);
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,7 +1,8 @@
 import login from "../controllers/login.js";
+import redisMiddleWare from "../middleware/redis.js";
 import { Router } from "express";
 
 const router = Router();
-router.route("/").post(login);
+router.route("/").post(redisMiddleWare, login);
 
 export default router;
